b3235e579ec73ed8877c6d5f49d130b70e5f1585
mega-dot <62636923+mega-dot@users.noreply.github.com>
2026-03-09 17:41:37 +1100

    Add files via upload

---
+++ app3.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 50;
+
+        if (number == 50)
+        {
+            Console.WriteLine("Number is 50");
+        }
+
+        Console.ReadLine();
+
+       
+        
+
+        
+
+
+    } }
+
+
+
+
+
